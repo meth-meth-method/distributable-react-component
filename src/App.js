9c03a80fc85995c61e6274bcb03a50514c51b223
Pontus Persson <pontus.alexander@gmail.com>
2017-08-14 07:23:07 +0200

    Add customization.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MyComponent from 'my-component';
 
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <MyComponent initiallyVisible/>
       </div>
     );
   }
